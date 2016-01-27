1632c3a3b748510fac56ecf5a96bcf8c82e9cde3
Sebastian Grunditz <nocoffein@gmail.com>
2016-01-27 20:04:31 +0100

    Begun tetris project

---
+++ src/se/liu/ida/sebgr273/tdd78/Board.java
@@ -0,0 +1,21 @@
+package se.liu.ida.sebgr273.tdd78;
+
+/**
+ * Created by Sebastian on 2016-01-27.
+ */
+public class Board {
+    private SquareType[][] squares;
+    private int width, height;
+
+    public Board(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.squares = new SquareType[height][width];
+
+        for(int width : )
+    }
+
+    public static void main(String[] args) {
+        Board board = new Board(20, 30);
+    }
+}
